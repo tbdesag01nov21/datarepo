d156d3dcd410d8ec71f432566bf08b500b06e1e3
erfederuiz <federicoruiz.ruiz@gmail.com>
2022-03-11 20:56:06 +0100

    Remove security info

--- aurora/first_connect.py
+++ aurora/first_connect.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import pymysql
 
-host="database-1.chxzwwbeyr7d.us-east-2.rds.amazonaws.com"
+host=""
 port=3306
 dbname="post"
-user="adminmysql"
-password="GaORNaXGqZPp6033jnYa"
+user=""
+password=""
 
 conn = pymysql.connect(host=host, user=user, port=port, passwd=password, db=dbname)
