3ea3d4b857857ca172f6dadf320d17786a679a52
erfederuiz <federicoruiz.ruiz@gmail.com>
2022-03-11 21:23:11 +0100

    Use aws_info.txt

--- aurora/first_connect.py
+++ aurora/first_connect.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import pymysql
 
-host=""
-port=3306
-dbname="post"
-user=""
-password=""
+def get_aws_creds():
+    myvars = {}
+    with open("aws_info.txt") as myfile:
+        for line in myfile:
+            name, var = line.partition("=")[::2]
+            myvars[name.strip()] = var.rstrip()
 
-conn = pymysql.connect(host=host, user=user, port=port, passwd=password, db=dbname)
+    return myvars
+
+aws_info = get_aws_creds()
+host = aws_info["host"]
+port=3306
+user = aws_info["user"]
+password = aws_info["password"]
+
+conn = pymysql.connect(host=host, user=user, port=port, passwd=password)
